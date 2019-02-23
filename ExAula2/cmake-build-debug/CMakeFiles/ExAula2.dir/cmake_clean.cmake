b989f0e8e69fdc558145383f69ddfb7536821f80
Fernanda <f.bezerra@alu.ufc.br>
2019-02-23 16:15:17 -0300

    It adds the file of the exercise of the second class

---
+++ ExAula2/cmake-build-debug/CMakeFiles/ExAula2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ExAula2.dir/main.cpp.obj"
+  "ExAula2.pdb"
+  "ExAula2.exe"
+  "ExAula2.exe.manifest"
+  "libExAula2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ExAula2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
