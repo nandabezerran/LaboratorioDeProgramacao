e4014ef1628f3906f5c28fb3c46d07be74e668a7
Fernanda Bezerra <fbn.prg@gmail.com>
2019-03-11 22:36:17 -0300

    [+] It adds the quick sort algorithm

--- ExAula4/main.cpp
+++ ExAula4/main.cpp
@@ -6,7 +6,8 @@ void Swap(int *p, int *p_){
     *p = *p_;
     *p_ = aux;
 }
-void LomutoPartitioning (int *init, int *fin, int *pivot){
+
+void LomutoPartitioning (int *init, int *fin, int *&pivot){
     Swap(init, pivot);
 
     int *i = init;
@@ -19,24 +20,50 @@ void LomutoPartitioning (int *init, int *fin, int *pivot){
         }
     }
     Swap(i, init);
-
+    pivot = i;
 }
 
-void PrintVector(int* vector){
+void PrintVector(int *vector){
     for (int i = 0; i < 9 ; ++i) {
         std::cout << vector[i] << " ";
     }
     std::cout << std::endl;
 }
 
+void QuickSort(int *init, int *fin, int *pivot){
+    int *initR;
+    int *finL;
+
+    if (fin <= init){
+        return;
+    }
+
+    LomutoPartitioning(init, fin, pivot);
+
+    if(pivot != init || pivot == fin){
+        finL = pivot - 1;
+        pivot = finL;
+        QuickSort(init, finL, pivot);
+    }
+
+    if(pivot == init || pivot != fin ){
+        initR = pivot + 1;
+        pivot = fin;
+        QuickSort(initR, fin, pivot);
+
+    }
+}
 int main() {
-    int vector[9] = {2,9,7,3,5,4,8,1,7};
+    int vector[] = {2,9,7,3,5,4,8,1,7};
     int *init = vector;
     int *fin = vector + 8;
     int *pivot = vector + 4;
 
     std::cout << "Pivot: " << *pivot << std::endl;
-    LomutoPartitioning(init, fin, pivot);
+    std::cout << "InitialVector:   ";
+    PrintVector(vector);
+    QuickSort(init, fin, pivot);
+    std::cout << "After QuickSort: ";
     PrintVector(vector);
 
     return 0;
