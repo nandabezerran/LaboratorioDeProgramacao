486d452d028208158ee383c6a3c5f53c61b722fa
Fernanda Bezerra <fbn.prg@gmail.com>
2019-03-25 20:01:10 -0300

    [+/-] It fixes the triple partitioning, it adds the return of this function as a tuple, and the quick sort using that algorithm

--- ExAula06/main.cpp
+++ ExAula06/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+using namespace std;
+#include<tuple>
 
 void Swap(int *p, int *p_){
     int aux;
@@ -7,38 +9,36 @@ void Swap(int *p, int *p_){
     *p_ = aux;
 }
 
-void LomutoPartitioning (int *init, int *fin, int *&pivot){
+tuple<int*, int*> LomutoPartitioningWithTriplePartitioning (int *init, int *fin, int *&pivot){
     Swap(init, pivot);
-
     int *i = init;
-    int *j = init + 1;
+    int *j = init+1;
     int *n = init;
-    int *p;
-
+    int p;
 
     for (j; j <= fin; ++j) {
         if(*j < *init){
-            i = i + 1;
-            *p = *i;
-            *i =*j;
+            n++;
+            p = *n;
+            *n = *j;
 
-            n = n+1;
-
-            *j = *j;
-            *n = *p;
+            i++;
+            *j = *i;
+            *i = p;
         }
 
-        if(*j == *init){
-            n = n + 1;
-            Swap(n,j);
+        else if(*j == *init){
+            i++;
+            Swap(i,j);
         }
     }
-    Swap(i, init);
+    Swap(n, init);
     pivot = i;
+    return make_tuple(n, i);
 }
 
 void PrintVector(int *vector){
-    for (int i = 0; i < 9 ; ++i) {
+    for (int i = 0; i < 10 ; ++i) {
         std::cout << vector[i] << " ";
     }
     std::cout << std::endl;
@@ -52,25 +52,25 @@ void QuickSort(int *init, int *fin, int *pivot){
         return;
     }
 
-    LomutoPartitioning(init, fin, pivot);
+    std::tuple<int*, int*> pivots = LomutoPartitioningWithTriplePartitioning(init, fin, pivot);
 
-    if(pivot != init || pivot == fin){
-        finL = pivot - 1;
+    if(get<0>(pivots) > init ){
+        finL = get<0>(pivots) - 1;
         pivot = finL;
         QuickSort(init, finL, pivot);
     }
 
-    if(pivot == init || pivot != fin ){
-        initR = pivot + 1;
+    if(get<1>(pivots) < fin ){
+        initR = get<1>(pivots) + 1;
         pivot = fin;
         QuickSort(initR, fin, pivot);
 
     }
 }
 int main() {
-    int vector[] = {2,9,7,3,5,4,8,1,7};
+    int vector[] = {10, 2, 6, 8, 6 , 6, 3, 4, 6, 5};
     int *init = vector;
-    int *fin = vector + 8;
+    int *fin = vector + 9;
     int *pivot = vector + 4;
 
     std::cout << "Pivot: " << *pivot << std::endl;
