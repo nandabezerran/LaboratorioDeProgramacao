7901a85d49573bd003261e34899fd985aef35838
Fernanda Bezerra <fbn.prg@gmail.com>
2019-04-09 11:10:01 -0300

    [+/-] It changes the main to make many instances of the principal vector, and run all versions of quick sort. When using the instance writer the BFPRT doesn't work

--- Trabalho01/main.cpp
+++ Trabalho01/main.cpp
@@ -9,27 +9,33 @@
 #include "QuickSortBfprt.h"
 #include "QuickSortMemory.h"
 #include "instancias_ruins_Quicksort.hpp"
+using namespace std::chrono;
+
+void CopyVector(const int vector[], int vectorAux[], int size){
+    for (int i = 0; i < size ; ++i) {
+        vectorAux[i] = vector[i];
+    }
+}
 
 int ChoosePivot_(int first, int last){
     int pivot = (last + first)/2;
     return pivot;
 }
 
-using namespace std::chrono;
-
-int* AleatoryVector(int* vector, int size){
+void AleatoryVector(int* vector, int size){
     srand((unsigned)time(nullptr));
     for (int i = 0; i < size; ++i) {
         vector[i] = rand() % 100;
     }
 
-    return vector;
 
 }
 
 int main() {
     int size;
     int option = 1;
+    int typeEntry;
+
     QuickSortIndex qSortI;
     QuickSortPointer qSortP;
     QuickSortRandomPivot qSortRp;
@@ -37,35 +43,131 @@ int main() {
     QuickSortMemory qSortMemory;
 
     while(option != 0) {
+        std::cout << "How do you want your vector?\n 0 - Aleatory Vector\n 1 - Instance generator " << std::endl;
+        std::cin >> typeEntry;
         std::cout << "Whats the size of the vector? " << std::endl;
         std::cin >> size;
         int vector[size];
-        bool aux = false; //= escrever_instancia(vector,size);
-        if (aux) {
-            std::cout << "There's not enough memory " << std::endl;
+        int vectorAux[size];
+        int *initP;
+        int *finP;
+        int initI;
+        int finI;
+
+        if (typeEntry == 0) {
+            AleatoryVector(vector, size);
+
         }
-        else {
-            int* init = AleatoryVector(vector, size);
-            int* fin = init + size - 1;
 
-            std::cout << "InitialVector:   ";
-            qSortBfprt.PrintVector_(vector, fin);
-            auto start = high_resolution_clock::now();
-            qSortBfprt.QuickSort_(init, fin);
-            auto stop = high_resolution_clock::now();
-            std::cout << "After QuickSort: ";
-            qSortBfprt.PrintVector_(vector, fin);
+        else if(typeEntry == 1){
+            bool aux = escrever_instancia(vector,size);
+            if (aux) {
+                std::cout << "There's not enough memory " << std::endl;
+            }
+            else {
+                escrever_instancia(vector, size);
 
-            auto duration = duration_cast<microseconds>(stop - start);
-            cout << "Time taken by function: "
-                 << duration.count() << " microseconds" << endl;
-
-            //delete init;
-            //delete fin;
+            }
         }
+
+        CopyVector(vector, vectorAux, size);
+        initP = vectorAux;
+        finP = initP + size - 1;
+
+        initI = 0;
+        finI = size - 1;
+
+        std::cout << "InitialVector:   ";
+        qSortBfprt.PrintVector_(vectorAux, finP);
+        std::cout << "\n";
+
+
+        /*  QUICK SORT INDEX
+         *
+         */
+        CopyVector(vector, vectorAux, size);
+        std::cout << "QuickSortIndex:\n";
+        auto start = high_resolution_clock::now();
+        qSortI.QuickSort_(initI, finI, vectorAux);
+        auto stop = high_resolution_clock::now();
+        std::cout << "After QuickSort: ";
+        qSortI.PrintVector_(vectorAux, finI);
+
+        auto duration = duration_cast<microseconds>(stop - start);
+        cout << "Time taken by QuickSortIndex: "
+             << duration.count() << " microseconds\n" << endl;
+
+
+        /*  QUICK SORT POINTER
+         *
+         */
+        CopyVector(vector, vectorAux, size);
+        std::cout << "QuickSortPointer:\n";
+        start = high_resolution_clock::now();
+        qSortP.QuickSort_(initP, finP);
+        stop = high_resolution_clock::now();
+        std::cout << "After QuickSort: ";
+        qSortP.PrintVector_(vectorAux, finP);
+
+        duration = duration_cast<microseconds>(stop - start);
+        cout << "Time taken by QuickSortPointer: "
+             << duration.count() << " microseconds\n" << endl;
+
+
+        /* QUICK SORT RANDOM PIVOT
+         *
+         */
+        CopyVector(vector, vectorAux, size);
+        std::cout << "QuickSortRandomPivot:\n";
+        start = high_resolution_clock::now();
+        qSortRp.QuickSort_(initP, finP);
+        stop = high_resolution_clock::now();
+        std::cout << "After QuickSort: ";
+        qSortRp.PrintVector_(vectorAux, finP);
+
+        duration = duration_cast<microseconds>(stop - start);
+        cout << "Time taken by QuickSortRandomPivot: "
+             << duration.count() << " microseconds\n" << endl;
+
+
+        /* QUICK SORT MEMORY
+         *
+         */
+        CopyVector(vector, vectorAux, size);
+        std::cout << "QuickSortMemory:\n";
+        start = high_resolution_clock::now();
+        qSortMemory.QuickSort_(initP, finP);
+        stop = high_resolution_clock::now();
+        std::cout << "After QuickSort: ";
+        qSortMemory.PrintVector_(vectorAux, finP);
+
+        duration = duration_cast<microseconds>(stop - start);
+        cout << "Time taken by QuickSortMemory: "
+             << duration.count() << " microseconds\n" << endl;
+
+
+        /* QUICK SORT BFPRT
+         *
+         */
+        CopyVector(vector, vectorAux, size);
+        std::cout << "QuickSortBfprt:\n";
+        start = high_resolution_clock::now();
+        qSortBfprt.QuickSort_(initP, finP);
+        stop = high_resolution_clock::now();
+        std::cout << "After QuickSort: ";
+        qSortBfprt.PrintVector_(vectorAux, finP);
+
+        duration = duration_cast<microseconds>(stop - start);
+        cout << "Time taken by QuickSortBfprt: "
+             << duration.count() << " microseconds\n" << endl;
+
+
+
+
         std::cout << "\nDo you want to enter another vector?\n   Press - Any number to continue"
                      "\n   Press - 0 to exit " << std::endl;
         std::cin >> option;
+
         if(option == 0){
             break;
         }
