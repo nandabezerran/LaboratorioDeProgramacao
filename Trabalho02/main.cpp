4a07bfdd31a4ef6ba578780b9943b369e1e6e463
Fernanda Bezerra <fbn.prg@gmail.com>
2019-04-25 10:34:52 -0300

    [+] It adds the computation of vector Pi and the algorithm of KMP

--- Trabalho02/main.cpp
+++ Trabalho02/main.cpp
@@ -10,37 +10,94 @@ void printVector(const int* vector){
 }
 
 void bruteForceAlgorithm(const char* pText, const char* pPattern, int *pResult){
-    int cont = 0;      // It counts in which index we are in the pattern;
-    int goThrough = 0; // It counts in which index we are in the text;
+    int goThroughPattern = 0;      // It counts in which index we are in the pattern;
+    int    goThroughText = 0;      // It counts in which index we are in the text;
+
     while(*pText != '\0'){
         if(*pText == *pPattern){
             pPattern = pPattern + 1;
-            cont++;
+            goThroughPattern++;
         }
 
         else{
-            pPattern = pPattern - cont;
+            pPattern = pPattern - goThroughPattern;
         }
 
         if(*pPattern == '\0'){
-            pPattern = pPattern - cont;
-            *pResult = goThrough - cont;
+            pPattern = pPattern - goThroughPattern;
+            *pResult = goThroughText - goThroughPattern;
             pResult = pResult + 1;
-            cont = 0;
+            goThroughPattern = 0;
         }
 
         pText = pText + 1;
-        goThrough++;
+        goThroughText++;
 
     }
     *pResult = -1;
+}
+int* computationOfPi(const char* pPattern, int* pPiVector){
+    int k;
+    int lenght;
+    *pPiVector = 0;
+    for (int i = 1; *(pPattern + i) != '\0' ; ++i) {
+        k = i - 1;
+        for(;;){
+            lenght = *(pPiVector + k);
+            if (lenght == 0) {
+                if (*pPattern == *(pPattern + i)) {
+                    *(pPiVector + i) = 1;
+                    break;
+                } else {
+                    *(pPiVector + i) = 0;
+                    break;
+                }
+            } else {
+                if (*(pPattern + i) == *(pPattern + lenght)) {
+                    *(pPiVector + i) = lenght + 1;
+                    break;
+                } else {
+                    k = lenght - 1;
+                }
+            }
+        }
+    }
+    return pPiVector;
+}
+
+void knuthMorrisPratt(const char* pText, const char* pPattern, int *pResult){
+    int *piVector =  new int[sizeof(pPattern)]();
+    computationOfPi(pPattern, piVector);
+    int goThroughText = 0;
+    int goThroughPattern = 0;
+    while(*(pText + goThroughText) != '\0'){
+        if(*(pText + goThroughText) != *(pPattern + goThroughPattern)){
+            if(goThroughPattern == 0){
+                goThroughText++;
+            }
+            else{
+                goThroughPattern = *(piVector + goThroughPattern - 1);
+            }
+        }
+        else{
+            if(*(pPattern + goThroughPattern) == '\0' ){
+                *pResult = goThroughText - goThroughPattern;
+                pResult = pResult + 1;
+                goThroughPattern = *(piVector + goThroughPattern);
+                goThroughText++;
+            }
+            else{
+                goThroughText++;
+                goThroughPattern++;
+            }
+        }
+    }
 }
 
 int main() {
 //    int textSize;
 //    int patternSize;
     int *result = new int[sizeof("o celito e o amor da minha vidda")]();
-
 //    std::cout << "Please, enter the size of the text " << std::endl;
 //    std::cin >> textSize;
 //    std::cout << "Please, enter the size of the pattern " << std::endl;
@@ -52,7 +109,9 @@ int main() {
     strcpy(pattern, "a");
 
     bruteForceAlgorithm(text,pattern,result);
+    printVector(result);
 
+    knuthMorrisPratt(text,pattern,result);
     printVector(result);
 
     return 0;
