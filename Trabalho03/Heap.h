7f6c3bb76f3c16987643f058600458c024641241
Fernanda Bezerra <fbn.prg@gmail.com>
2019-05-30 17:14:33 -0300

    [+]It reads the files and adds the frequency of each char in a dictionary

---
+++ Trabalho03/Heap.h
@@ -0,0 +1,35 @@
+//
+// Created by fbeze on 30/05/2019.
+//
+
+#ifndef TRABALHO03_HEAP_H
+#define TRABALHO03_HEAP_H
+
+#include <stdio.h>
+
+using namespace std;
+
+//class Heap {
+//public:
+//
+//    int heapSize;
+//    int* heap;
+//
+//    void buildHeap(int *pVector, int pSize);
+//    void heapify(int* pVector, int pIndex);
+//
+//    int getParent(int pIndex);
+//
+//    int getLeft(int pIndex);
+//
+//    int getRight(int pIndex);
+//
+//    void insert(int x);
+//
+//    int minimum();
+//
+//    int extractMinimum();
+//
+//    void Swap(int *p, int pIndex, int largest);
+//};
+#endif //TRABALHO03_HEAP_H
---
+++ Trabalho03/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <map>
+#include "Heap.cpp"
+#include <fstream>
+typedef map<char, int> dict;
+typedef pair<char, int> par;
+
+dict countChar(dict pDict, string pText){
+    int i = 0;
+    while(pText[i] != '\0'){
+        if (pDict[pText[i]])
+            pDict[pText[i]] += 1;
+        else
+            pDict[pText[i]] = 1;
+        i++;
+    }
+    return pDict;
+}
+
+dict readFile(const string pFileName){
+    ifstream inputFile(pFileName);
+    dict dictFile;
+    string fileLine;
+    cout << pFileName << endl;
+    if (!inputFile){
+        cout << "Failed to open file\n";
+        return dictFile;
+    }
+
+    while (std::getline(inputFile, fileLine))
+        dictFile = countChar(dictFile, fileLine);
+
+    return dictFile;
+}
+
+
+int main() {
+    string fileName = "Test.txt";
+    dict test = readFile(fileName);
+
+    for(par element: test){
+        cout << element.first << " : " << element.second << endl;
+    }
+
+    return 0;
+}
