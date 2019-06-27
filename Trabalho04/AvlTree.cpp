806a479f7e127fe148634da845003d7645f06732
Fernanda Bezerra <fbn.prg@gmail.com>
2019-06-27 19:22:20 -0300

    Insertion and deletion working, but have to change the value to key

--- Trabalho04/AvlTree.cpp
+++ Trabalho04/AvlTree.cpp
@@ -7,17 +7,26 @@
 #include "AvlTree.hpp"
 using namespace std;
 
-//Noh* procurar(DicAVL &D, TC c) {
-//    if (c == null && D.raiz == nullptr)
-//        return -1;
-//
-//    if (D.raiz->chave == c && D.raiz == nullptr)
-//        return D.raiz;
-//    else if (D.raiz->chave > chave)
-//        return search(c, D.raiz->esq);
-//    else
-//        return search(c,  D.raiz->dir);
-//}
+Noh* procurar(DicAVL &D, TC c) {
+    Noh* nohAtual = D.raiz;
+    if (nohAtual == nullptr)
+        return nullptr;
+
+    while(nohAtual->chave != c){
+        if(nohAtual->valor > c) {
+            nohAtual = nohAtual->esq;
+        }
+        else if(nohAtual->valor < c){
+            nohAtual = nohAtual->dir;
+        }
+
+        if(nohAtual == nullptr){
+            cout << "Noh nao encontrado" << endl;
+            return nullptr ;
+        }
+    }
+    return nohAtual;
+}
 
 void inicializar (DicAVL &D){
     D.raiz = nullptr;
@@ -51,18 +60,29 @@ Noh* rotacaoDireita(Noh *n){
     Noh* x = n->esq;
     Noh* DirX = x->dir;
 
+    if(n->pai != nullptr) {
+        if(n->pai->dir == n) {
+            n->pai->dir = x;
+        } else {
+            n->pai->esq = x;
+        }
+    }
+
     x->dir = n;
     n->esq = DirX;
+    if(DirX != nullptr) {
+        DirX->pai = n;
+    }
+    x->pai = n->pai;
+    n->pai = x;
 
-    if(n->esq->h > (n->dir->h + 1))
-        n->h = n->esq->h;
-    else
-        n->h = n->dir->h + 1;
+    int nEsqH = n->esq != nullptr ? n->esq->h + 1 : 1;
+    int nDirH = n->dir != nullptr ? n->dir->h + 1 : 1;
+    n->h = max(nDirH, nEsqH);
 
-    if(x->esq->h > (x->dir->h + 1))
-        x->h = x->esq->h;
-    else
-        x->h = x->dir->h + 1;
+    int xEsqH = x->esq != nullptr ? x->esq->h + 1 : 1;
+    int xDirH = x->dir != nullptr ? x->dir->h + 1 : 1;
+    x->h = max(xDirH, xEsqH);
     return x;
 }
 
@@ -70,85 +90,288 @@ Noh* rotacaoEsquerda(Noh *x){
     Noh* y = x->dir;
     Noh* EsqY = y->esq;
 
-    x->esq = EsqY;
-    y->dir = x;
+    if(x->pai != nullptr) {
+        if(x->pai->dir == x) {
+            x->pai->dir = y;
+        } else {
+            x->pai->esq = y;
+        }
+    }
 
-    if(y->esq->h > (y->dir->h + 1))
-        y->h = y->esq->h;
-    else
-        y->h = y->dir->h + 1;
+    x->dir = EsqY;
+    if(EsqY != nullptr) {
+        EsqY->pai = x;
+    }
+    y->esq = x;
+    y->pai = x->pai;
+    x->pai = y;
 
-    if(x->esq->h > (x->dir->h + 1))
-        x->h = x->esq->h;
-    else
-        x->h = x->dir->h + 1;
+    int xEsqH = x->esq != nullptr ? x->esq->h + 1 : 1;
+    int xDirH = x->dir != nullptr ? x->dir->h + 1 : 1;
+    x->h = max(xDirH, xEsqH);
+
+    int yEsqH = y->esq != nullptr ? y->esq->h + 1 : 1;
+    int yDirH = y->dir != nullptr ? y->dir->h + 1 : 1;
+    y->h = max(yEsqH, yDirH);
 
     return y;
 }
 
-int maximun(int a, int b){
-    return (a > b)? a : b;
-}
-
-int balance(int a, int b){
-    return a - b;
-}
-
 Noh* inserir (DicAVL &D, TC c, TV v){
-    Noh* actualNode = D.raiz;
-    Noh* actualFather = nullptr;
-    int balanceFactor = 0;
+    Noh* nohAtual = D.raiz;
+    Noh* paiAtual = nullptr;
 
-    if(actualNode == nullptr){
-        actualNode = new Noh;
-        actualNode->chave = c;
-        actualNode->valor = v;
-        actualNode->h = 1;
-        actualFather = nullptr;
-        return actualNode;
+    // First node insertion
+    if(nohAtual == nullptr){
+        nohAtual = new Noh();
+        nohAtual->chave = c;
+        nohAtual->valor = v;
+        nohAtual->h = 1;
+        nohAtual->dir = nullptr;
+        nohAtual->esq = nullptr;
+        nohAtual->pai = nullptr;
+        D.raiz = nohAtual;
+        return nohAtual;
     }
-    bool isNull = false;
-    while(!isNull){
-        if (c < actualNode->chave) {
-            actualFather = actualNode;
-            actualNode = actualNode->esq;
-        }
 
-        else if (c >= actualNode->chave) {
-            actualFather = actualNode;
-            actualNode = actualNode->dir;
-
-        }
-
-        if (actualNode == nullptr) {
-            isNull = true;
-            actualNode = new Noh;
-            actualNode->chave = c;
-            actualNode->valor = v;
-            actualNode->h = 1;
-            actualNode->pai = actualFather;
-            actualFather->h = 1 + maximun(actualFather->dir->h, actualFather->esq->h);
-            balanceFactor = balance(actualFather->dir->h, actualFather->esq->h);
-            // Left Left Case
-            if (balanceFactor > 1 && v < actualFather->esq->valor)
-                return rotacaoDireita(actualFather);
-
-            // Right Right Case
-            if (balanceFactor < -1 && v > actualFather->dir->valor)
-                return rotacaoEsquerda(actualFather);
-
-            // Left Right Case
-            if (balanceFactor > 1 && v < actualFather->esq->valor){
-                actualFather->esq = rotacaoEsquerda(actualFather->esq);
-                return rotacaoDireita(actualFather);
-            }
-
-            // Right Left Case
-            if (balanceFactor < -1 && v > actualFather->dir->valor){
-                actualFather->dir = rotacaoDireita(actualFather->dir);
-                return rotacaoEsquerda(actualFather);
-            }
+    // Search the tree position;
+    bool praEsq;
+    while(nohAtual != nullptr) {
+        paiAtual = nohAtual;
+        praEsq = nohAtual->valor > v;
+        nohAtual->h++;
+        if(praEsq) {
+            nohAtual = nohAtual->esq;
+        } else {
+            nohAtual = nohAtual->dir;
         }
     }
-    return actualNode;
+
+    // Create the new node
+    Noh* novoNoh = new Noh();
+    novoNoh->h = 1;
+    novoNoh->chave = c;
+    novoNoh->valor = v;
+    novoNoh->pai = paiAtual;
+    novoNoh->dir = nullptr;
+    novoNoh->esq = nullptr;
+
+    // Insert in the tree;
+    if(praEsq) {
+        paiAtual->esq = novoNoh;
+    } else {
+        paiAtual->dir = novoNoh;
+    }
+
+    int hEsq;
+    int hDir;
+    // Run back the tree and find unbalance;
+    do {
+        hEsq = paiAtual->esq != nullptr ? paiAtual->esq->h : 0;
+        hDir = paiAtual->dir != nullptr ? paiAtual->dir->h : 0;
+
+        if((hEsq + 1) != paiAtual->h && (hDir + 1) != paiAtual->h) {
+            paiAtual->h = max(hEsq + 1, hDir + 1);
+        }
+
+        if(hEsq - hDir > 1) {
+            // Left pre left rot
+            if(paiAtual->esq != nullptr) {
+                hEsq = paiAtual->esq->esq != nullptr ? paiAtual->esq->esq->h : 0;
+                hDir = paiAtual->esq->dir != nullptr ? paiAtual->esq->dir->h : 0;
+                if(hDir > hEsq) {
+                    rotacaoEsquerda(paiAtual->esq);
+                }
+            }
+            // Right Rotation;
+            paiAtual = rotacaoDireita(paiAtual);
+            if(paiAtual->pai == nullptr) {
+                D.raiz = paiAtual;
+            }
+        } else if (hEsq - hDir < -1) {
+            // Right Pre left rot
+            if(paiAtual->dir != nullptr) {
+                hEsq = paiAtual->dir->esq != nullptr ? paiAtual->dir->esq->h : 0;
+                hDir = paiAtual->dir->dir != nullptr ? paiAtual->dir->dir->h : 0;
+                if(hEsq > hDir) {
+                    rotacaoDireita(paiAtual->dir);
+                }
+            }
+            // Left Rotation;
+            paiAtual = rotacaoEsquerda(paiAtual);
+            if(paiAtual->pai == nullptr) {
+                D.raiz = paiAtual;
+            }
+        } else {
+            paiAtual = paiAtual->pai;
+        }
+    } while(paiAtual != nullptr);
+
+    return novoNoh;
+}
+
+Noh* acharSucessor(Noh* n){
+    Noh* nohAtual = n->dir;
+    while (nohAtual->esq != nullptr)
+        nohAtual = nohAtual->esq;
+    return nohAtual;
+}
+
+void ajustarAlturaArvore(Noh* paiAtual){
+    if(!paiAtual){
+        return;
+    }
+    int hEsq;
+    int hDir;
+    hEsq = paiAtual->esq != nullptr ? paiAtual->esq->h : 0;
+    hDir = paiAtual->dir != nullptr ? paiAtual->dir->h : 0;
+    if((hEsq + 1) != paiAtual->h && (hDir + 1) != paiAtual->h) {
+        paiAtual->h = max(hEsq + 1, hDir + 1);
+    }
+    ajustarAlturaArvore(paiAtual->pai);
+}
+
+void remover (DicAVL &D, Noh *n){
+    Noh* nohAtual = D.raiz;
+    Noh* paiAtual = nullptr;
+
+    if(!nohAtual){
+        cout << "Arvore vazia" << endl;
+        return;
+    }
+
+    while(nohAtual->valor != n->valor){
+        if(nohAtual->valor > n->valor) {
+            nohAtual = nohAtual->esq;
+        }
+        else if(nohAtual->valor < n->valor){
+            nohAtual = nohAtual->dir;
+        }
+
+        if(nohAtual == nullptr){
+            cout << "Noh nao encontrado" << endl;
+            return;
+        }
+    }
+
+    paiAtual = nohAtual->pai;
+    bool esquerda = true;
+    if(paiAtual->dir == nohAtual){
+        esquerda = false;
+    }
+    if(nohAtual->dir == nullptr && nohAtual->esq == nullptr){
+        free(nohAtual);
+        if(esquerda){
+            paiAtual->esq = nullptr;
+        }
+        else{
+            paiAtual->dir = nullptr;
+        }
+        ajustarAlturaArvore(paiAtual);
+    }
+
+    else if(nohAtual->dir == nullptr){
+        if(esquerda){
+            paiAtual->esq = nohAtual->esq;
+            nohAtual->esq->pai = paiAtual;
+            cout << paiAtual->h << endl;
+            ajustarAlturaArvore(paiAtual);
+            cout << paiAtual->h << endl;
+            free(nohAtual);
+        }
+        else{
+            paiAtual->dir = nohAtual->esq;
+            nohAtual->esq->pai = paiAtual;
+            cout << paiAtual->h << endl;
+            ajustarAlturaArvore(paiAtual);
+            cout << paiAtual->h << endl;
+            free(nohAtual);
+        }
+    }
+    else if(nohAtual->esq == nullptr) {
+        if (esquerda) {
+            paiAtual->esq = nohAtual->dir;
+            nohAtual->dir->pai = paiAtual;
+            ajustarAlturaArvore(paiAtual);
+            free(nohAtual);
+        } else {
+            paiAtual->dir = nohAtual->dir;
+            nohAtual->dir->pai = paiAtual;
+            ajustarAlturaArvore(paiAtual);
+            free(nohAtual);
+        }
+    }
+    else {
+        Noh *sucessor = acharSucessor(nohAtual);
+        Noh *auxEsq;
+        Noh *auxDir;
+        int auxH;
+        Noh *auxPai;
+        if(esquerda){
+            paiAtual->esq = sucessor;
+        }
+        else{
+            paiAtual->dir = sucessor;
+        }
+        auxPai = sucessor->pai;
+        auxEsq = nohAtual->esq;
+        auxDir = nohAtual->dir;
+        auxH = nohAtual->h;
+
+        auxPai->esq = sucessor->dir;
+
+        sucessor->pai->esq = nohAtual;
+        sucessor->dir = auxDir;
+        sucessor->esq = auxEsq;
+        sucessor->pai = paiAtual;
+        sucessor->h = auxH;
+
+        free(nohAtual);
+        auxPai->esq = nullptr;
+        ajustarAlturaArvore(auxPai);
+    }
+
+    int hEsq;
+    int hDir;
+    // Run back the tree and find unbalance;
+    do {
+        hEsq = paiAtual->esq != nullptr ? paiAtual->esq->h : 0;
+        hDir = paiAtual->dir != nullptr ? paiAtual->dir->h : 0;
+
+        if((hEsq + 1) != paiAtual->h && (hDir + 1) != paiAtual->h) {
+            paiAtual->h = max(hEsq + 1, hDir + 1);
+        }
+
+        if(hEsq - hDir > 1) {
+            // Left pre left rot
+            if(paiAtual->esq != nullptr) {
+                hEsq = paiAtual->esq->esq != nullptr ? paiAtual->esq->esq->h : 0;
+                hDir = paiAtual->esq->dir != nullptr ? paiAtual->esq->dir->h : 0;
+                if(hDir > hEsq) {
+                    rotacaoEsquerda(paiAtual->esq);
+                }
+            }
+            // Right Rotation;
+            paiAtual = rotacaoDireita(paiAtual);
+            if(paiAtual->pai == nullptr) {
+                D.raiz = paiAtual;
+            }
+        } else if (hEsq - hDir < -1) {
+            // Right Pre left rot
+            if(paiAtual->dir != nullptr) {
+                hEsq = paiAtual->dir->esq != nullptr ? paiAtual->dir->esq->h : 0;
+                hDir = paiAtual->dir->dir != nullptr ? paiAtual->dir->dir->h : 0;
+                if(hEsq > hDir) {
+                    rotacaoDireita(paiAtual->dir);
+                }
+            }
+            // Left Rotation;
+            paiAtual = rotacaoEsquerda(paiAtual);
+            if(paiAtual->pai == nullptr) {
+                D.raiz = paiAtual;
+            }
+        } else {
+            paiAtual = paiAtual->pai;
+        }
+    } while(paiAtual != nullptr);
 }
--- Trabalho04/main.cpp
+++ Trabalho04/main.cpp
@@ -1,18 +1,93 @@
 #include <iostream>
+#include <iomanip>
+#include <c++/cmath>
 #include "AvlTree.hpp"
 #include "AvlTree.cpp"
 
+int tamValor = 2;
+int distancia = 2;
+
+TV pegarValorPraImpressao(DicAVL &D, int profundidade, int indice) {
+    Noh* nohAtual = D.raiz;
+
+    for(int i = profundidade - 1; i >= 0; i--) {
+        if((indice >> i) & 1) {
+            nohAtual = nohAtual->dir;
+        } else {
+            nohAtual = nohAtual->esq;
+        }
+        if(nohAtual == nullptr) {
+            return 0;
+        }
+    }
+    return nohAtual->valor;
+
+}
+
+void imprimirArvore(DicAVL &D) {
+    if(D.raiz == nullptr) {
+        cout << "[" << std::string( tamValor, ' ' ) << "]" << endl << endl;
+        return;
+    }
+    for(int i = 0; i < D.raiz->h; i++) {
+        int nohs = (int)pow(2, (D.raiz->h-1) - i);
+        int colunas = nohs * (tamValor + 2) + (nohs - 1) * distancia;
+        int espacoInicial = (colunas / 2) - ((tamValor + 2) / 2);
+        int espacoEntre = distancia + ((distancia + tamValor + 2) * ((int)(pow(2, (D.raiz->h-1) - i)) - 1));
+        cout << std::string(espacoInicial, ' ');
+        int nosNessaLinha = (int)pow(2, i);
+        for(int j = 0; j < nosNessaLinha; j++) {
+            if(j != 0) {
+                cout << std::string(espacoEntre, ' ');
+            }
+            TV valor = pegarValorPraImpressao(D, i, j);
+            if(valor == 0) {
+                cout << "[" << std::string(tamValor, ' ') << "]";
+            } else {
+                cout << "[" << setfill('0') << setw(tamValor) << valor << "]";
+            }
+        }
+        cout << endl;
+    }
+    cout << endl << "---------" << endl;
+}
 
 int main() {
     DicAVL dictTest;
     Noh* no;
+    Noh* no1;
+
     inicializar(dictTest);
-    no = inserir(dictTest, 1, 10);
-    no = inserir(dictTest, 1, 20);
-    no = inserir(dictTest, 1, 30);
-    no = inserir(dictTest, 1, 40);
-    no = inserir(dictTest, 1, 50);
-    no = inserir(dictTest, 1, 25);
-    std::cout << "Hello, World!" << std::endl;
+    no = inserir(dictTest, 1, 2);
+    imprimirArvore(dictTest);
+    no1 = inserir(dictTest, 2, 1);
+    imprimirArvore(dictTest);
+    no = inserir(dictTest, 3, 4);
+    imprimirArvore(dictTest);
+    no = inserir(dictTest, 4, 3);
+    imprimirArvore(dictTest);
+    no = inserir(dictTest, 5, 5);
+    imprimirArvore(dictTest);
+//    no = inserir(dictTest, 6, 48);
+//    imprimirArvore(dictTest);
+//    no = inserir(dictTest, 7, 62);
+//    imprimirArvore(dictTest);
+//    no = inserir(dictTest, 8, 78);
+//    imprimirArvore(dictTest);
+//    no = inserir(dictTest, 9, 88);
+//    imprimirArvore(dictTest);
+//    no = inserir(dictTest, 10, 92);
+//    imprimirArvore(dictTest);
+//    no = inserir(dictTest, 10, 80);
+//    imprimirArvore(dictTest);
+//    no1 = inserir(dictTest, 10, 82);
+//    imprimirArvore(dictTest);
+//    no = inserir(dictTest, 10, 84);
+//    imprimirArvore(dictTest);
+
+
+    remover(dictTest, no1);
+    imprimirArvore(dictTest);
+    std::cout << "" << std::endl;
     return 0;
 }
